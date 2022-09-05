62b5805d960bd3254b7fdd36a821cfe29a910a43
Ozan Yetkin <ozan.yetkin@metu.edu.tr>
2022-09-05 17:48:52 +0300

    Yet another hello world added

--- CSWorksheet/Program.cs
+++ CSWorksheet/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HelloWorld;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+HelloWorld.Program.Main();
